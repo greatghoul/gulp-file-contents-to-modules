2bf253a07813c36a1c79b500b6564217cba7b6f3
vsync <vsync.design@gmail.com>
2016-03-05 19:20:46 +0200

    improvements to code - added the ability to minify output string - added the ability to wrap the string in a function (for compilation)

--- index.js
+++ index.js
@@ -3,30 +3,48 @@
 var through2 = require('through2');
 var gutil = require('gulp-util');
 var path = require('path');
-var fs = require('fs');
-
 
 var PLUGIN_NAME = 'gulp-file-contents-to-modules';
 
-module.exports = function (dest) {
+module.exports = function (options) {
+  options = options || {};
 
-  var output = '';
   var first = null;
-  var guid = 'xxxxxxxx-xxxx-4xxx-yxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random()*16|0,v=c==='x'?r:r&0x3|0x8;return v.toString(16);
-  });
+
+  function createExport( file ){
+      var output = '';
+      var name = file.path.replace(file.base, '').replace(/\\/g,'__').replace(/-/g,'_');   // 'foo/bar/baz-meh.txt' => 'foo__bar__baz_meh'
+
+      // now I added the below line which removed the DOT extension
+      output += 'export var ' + name.split('.').slice(0, -1).join('.') + ' = ';
+      var contents = file.contents.toString("utf-8");
+
+      if( options.minify )
+          contents = contents.replace( /\s[\r\n ]+/g, '' ) // remove new lines
+                             .replace(/>\s+</g, "><");     // remove whitespaces between tags
+
+      if( options.compile === true )
+        output += '_.template(' + JSON.stringify(contents, null, 1) + ');';
+      else if( options.compile )
+        output += options.compile +'(' + JSON.stringify(contents, null, 1) + ');';
+      else
+        output += JSON.stringify(contents, null, 1) + ';';
+
+      return output;
+  }
 
   //
   // Setup the stream to be returned.
   // through2.obj(fn) is a convenience wrapper around
   // through2({ objectMode: true }, fn)
   //
-  return through2.obj(function (file, enc, callback) {
+  return through2.obj(function( file, enc, callback ){
 
     //
     // Always error if file is a stream or null.
     //
     if ( file.isNull() ) {
+      this.push(file);
       return callback();
     }
     else if ( file.isStream() ) {
@@ -35,31 +53,28 @@ module.exports = function (dest) {
     }
 
     try {
-
       first = first || file;
-      var id = file.path.replace(file.base, '').replace(/\/\//g,'_').replace(/-/g,'_');   // 'foo/bar/baz-meh.txt' => 'foo__bar__baz_meh'
-      // now I added the below line which removed the DOT extension
-      id = 'export var ' + id.split('.').slice(0, -1).join('.'); 
-      var contents = file.contents.toString("utf-8");
-      
-      output += id + ' = ' + JSON.stringify(contents, null, 1) + ';\n'
-        
+
       //
       // Create file which will "host" the templates' exports.
       //
-      var out = new gutil.File({
-        base: first.base,
-        cwd: first.cwd,
-        path: path.join(file.base, dest),
-        contents: new Buffer(output)
-      });
+      file.contents = new Buffer( createExport(file) );
+      file.path = gutil.replaceExtension(file.path, '.js');
 
-      this.push(out);
-      callback();
-    } catch (e) {
-      this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Error:', e));
+      // var out = new gutil.File({
+      //   base: first.base,
+      //   cwd: first.cwd,
+      //   path: path.join(file.base, options.dest),
+      //   contents: new Buffer(output)
+      // });
+    } catch (err) {
+      // this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Error:', err));
+      this.emit('error', new PluginError(PLUGIN_NAME, err, {fileName: filePath}));
       callback(e);
     }
+
+    this.push(file);
+    callback();
 
   });
 
